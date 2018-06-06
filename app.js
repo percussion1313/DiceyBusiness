873202e7b3de115843458f1bf5968df79b4ab98d
percussion1313 <peterjacobgrant@gmail.com>
2018-06-06 15:49:26 -0500

    Fixed dice adding function and random variable function

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-let dieButtion = document.getElementById('die-button');
+let dieButtion = document.getElementById('generate-button');
 dieButtion.addEventListener('click', insertDice);
+
+const dieOptions = [1, 2, 3, 4, 5, 6]
 
 
 class Dice {
-    constructor(x,y,z) {
+    constructor(x,y,z, value) {
         this.div = document.createElement('div')
         this.div.style.height = (x);
         this.div.style.width = (y);
         this.div.style.border = (z);
+        this.div.textContent = (value)
         this.div.classList.add('dice');
         document.body.appendChild(this.div);
     }
@@ -16,6 +19,11 @@ class Dice {
 function insertDice() {
     let xVal = ('175px');
     let yVal = ('175px');
-    let border = ('3px solid black')
-    let newDice = new Dice(xVal, yVal, border);
+    let border = ('4px solid black')
+    let dieNumber = (randomDie())
+    let newDie = new Dice(xVal, yVal, border, dieNumber);
+}
+
+function randomDie() {
+    return Math.floor(Math.random() * dieOptions.length); 
 }
