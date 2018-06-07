aeb592da9e29b654d9394d07ad3a0e2b0510dfd7
percussion1313 <peterjacobgrant@gmail.com>
2018-06-07 10:32:18 -0500

    Added roll() function and tried calling it inside another function, however it is not recognizing it as a function

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 let dieButtion = document.getElementById('generate-button');
 dieButtion.addEventListener('click', insertDice);
 
-const dieOptions = [1, 2, 3, 4, 5, 6]
+let dieOptions = [1, 2, 3, 4, 5, 6]
 
 
-class Dice {
-    constructor(x,y,z, value) {
+class Die {
+    constructor(x, y, z, val) {
         this.div = document.createElement('div')
         this.div.style.height = (x);
         this.div.style.width = (y);
         this.div.style.border = (z);
-        this.div.textContent = (value)
-        this.div.classList.add('dice');
+        this.div.innerText = (val)
+        this.div.classList.add('die');
         document.body.appendChild(this.div);
+        this.roll();
+
+    }
+    roll() {
+        let value = Math.floor(Math.random() * 6 + 1);
+        this.div.innerText = value;
     }
 }
 
 function insertDice() {
-    let xVal = ('175px');
-    let yVal = ('175px');
+    let height = ('175px');
+    let width = ('175px');
     let border = ('4px solid black')
     let dieNumber = (randomDie())
-    let newDie = new Dice(xVal, yVal, border, dieNumber);
+    let newDie = new Die(height, width, border, dieNumber);
 }
 
 function randomDie() {
-    return Math.floor(Math.random() * dieOptions.length); 
+    return Math.floor(Math.random() * 6 + 1);
+}
+
+let rollButton = document.getElementById('roll-button');
+rollButton.addEventListener('click', rollDice);
+
+function rollDice() {
+    for (let i = 0; i < dieOptions.length; i++) {
+        dieOptions[i].roll();
+    }
 }
